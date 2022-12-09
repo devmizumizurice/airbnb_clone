ffb7b50453c9672bc65e2d38f90294fbfe1daae4
みずまんま <app.mizumizurice@gmail.com>
2022-12-09 13:34:53 +0900

    12/09 13:34

--- components/Banner.js
+++ components/Banner.js
@@ -15,7 +15,7 @@ function Banner() {
                 <p class='text-sm sm:text-lg text-white'>Not sure where to go? Perfect.</p>
                 <button class='text-purple-500 bg-white px-10 py-4
                 shadow-md rounded-full font-bold my-3
-                hover:shadow-xl active:scale-90 transition duration-150'>I'm flexible</button>
+                hover:shadow-xl active:scale-90 transition duration-150'>I am flexible</button>
             </div>
         </div>
     )
