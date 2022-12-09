f9b241eeedf2e8ca11aeefaa65676acadb2b381d
みずまんま <app.mizumizurice@gmail.com>
2022-12-09 13:39:52 +0900

    12/09 13:34

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import Banner from '../components/Banner';
-import Footer from '../components/footer';
+import Footer from '../components/Footer';
 import Header from '../components/Header';
 import LargeCard from '../components/LargeCard';
 import MediumCard from '../components/MediumCard';
